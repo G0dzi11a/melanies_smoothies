74d225c771ee81f7edf5b2d36a24b2dcdc62b794
G0dzi11a <bigslick_076@yahoo.com>
2025-01-05 19:12:36 -0600

    Update streamlit_app.py
    import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 import requests
+import pandas
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
